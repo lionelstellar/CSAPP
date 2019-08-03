2e82293564089c848ab77b5c5b3977a352252eb5
lionelstellar <1023612737@qq.com>
2019-08-03 10:17:20 +0800

    8-3

---
+++ DataLab/bitAnd.c
@@ -0,0 +1,18 @@
+/*
+ * bitAnd - x&y using only ~ and |
+ *   Example: bitAnd(6, 5) = 4
+ *   Legal ops: ~ |
+ *   Max ops: 8
+ *   Rating: 1
+ */
+#include <stdio.h>
+int bitAnd(int x, int y){
+    printf("%d\n", x);
+    return 0;
+}
+
+int main(){
+    return 0;
+}
+
+
